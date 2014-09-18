e835d9f615c6da4393996d258906b2a82b792ca1
Damian Palpacelli <damian.palpacelli@gmail.com>
2014-09-17 21:29:44 -0300

    add real logic for adding Workers checking duplication

--- service-domain/src/main/java/reparationservice/ReparationService.java
+++ service-domain/src/main/java/reparationservice/ReparationService.java
@@ -1,16 +1,30 @@
 package reparationservice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import reparationservice.entities.Worker;
 
 public class ReparationService {
+	private List<Worker> workers = new ArrayList<Worker>();
+	
+	public class WorkerAlreadyExists extends RuntimeException {
+		private static final long serialVersionUID = -478576357592578461L;
+	}
+
 
 	public void add(Worker worker) {
-		// TODO Auto-generated method stub
-		
+		if(workers.contains(worker))
+			throw new WorkerAlreadyExists();
+		workers.add(worker);
 	}
-
-	public Worker getWorker(Worker worker) {
-		return worker;
+	
+	public Worker getWorkerByUserName(String workerUserName) {
+		for (Worker worker : workers) {
+			if (worker.getUserName().equals(workerUserName))
+				return worker;
+		}
+			
+		return null;
 	}
-
 }
--- service-domain/src/test/java/reparationservice/ReparationServiceTests.java
+++ service-domain/src/test/java/reparationservice/ReparationServiceTests.java
@@ -8,7 +8,8 @@ import org.junit.Test;
 import reparationservice.entities.Worker;
 
 public class ReparationServiceTests {
-	private static final String WORKER_USER_NAME = "UserName";
+	private static final String WORKER_USER_NAME_1 = "UserName1";
+	private static final String WORKER_USER_NAME_2 = "UserName2";
 	private ReparationService reparationService;
 
 	@Before
@@ -18,13 +19,35 @@ public class ReparationServiceTests {
 
 	@Test
 	public void addWorkerToReparationService() {
-		Worker worker = newWorker();
-		reparationService.add(worker);
-		
-		assertThat(reparationService.getWorker(worker)).isEqualTo(newWorker());
+		reparationService.add(newWorker1());
+		reparationService.add(newWorker2());
+
+		Worker worker1 = reparationService
+				.getWorkerByUserName(WORKER_USER_NAME_1);
+		assertThat(worker1.getUserName()).isEqualTo(WORKER_USER_NAME_1);
+
+		Worker worker2 = reparationService
+				.getWorkerByUserName(WORKER_USER_NAME_2);
+		assertThat(worker2.getUserName()).isEqualTo(WORKER_USER_NAME_2);
 	}
 
-	private Worker newWorker() {
-		return Worker.newInstance(WORKER_USER_NAME);
+	@Test(expected = ReparationService.WorkerAlreadyExists.class)
+	public void throwWorkerAlreadyExistsWhenTwoWorkersWithSameUserNameAdded() {
+		reparationService.add(newWorker1());
+		reparationService.add(newWorker1());
+	}
+
+	@Test
+	public void returnNullWhenWorkerNotFound() {
+		assertThat(reparationService.getWorkerByUserName(WORKER_USER_NAME_2))
+				.isNull();
+	}
+
+	private Worker newWorker1() {
+		return Worker.newInstance(WORKER_USER_NAME_1);
+	}
+
+	private Worker newWorker2() {
+		return Worker.newInstance(WORKER_USER_NAME_2);
 	}
 }
