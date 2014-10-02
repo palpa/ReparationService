bccebf5b33157cb471f7d6a5e373884b49350239
Damian Palpacelli <damian.palpacelli@gmail.com>
2014-10-01 23:11:15 -0300

    Use HierarchicalContextRunner for AddReparationInteractorTest   This separates test without Customer added

--- service-domain/src/test/java/reparationservice/interactors/AddReparationInteractorTest.java
+++ service-domain/src/test/java/reparationservice/interactors/AddReparationInteractorTest.java
@@ -6,12 +6,15 @@ import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import de.bechte.junit.runners.context.HierarchicalContextRunner;
 import reparationservice.ReparationService;
 import reparationservice.entities.Customer;
 import reparationservice.entities.Reparation;
 import reparationservice.gateways.CustomerGateway;
 
+@RunWith(HierarchicalContextRunner.class)
 public class AddReparationInteractorTest {
 	private Interactor addReparation;
 	private CustomerGateway customers;
@@ -25,29 +28,37 @@ public class AddReparationInteractorTest {
 		String failure = "";
 		String urgency = "";
 		String observations = "";
-		addReparation = new AddReparationInteractor(creationDate, failure, urgency, observations, customerId, deviceSerialNumber, customers);
+		addReparation = new AddReparationInteractor(creationDate, failure,
+				urgency, observations, customerId, deviceSerialNumber,
+				customers);
 	}
 
-	@Ignore
-	@Test
-	public void testAddReparation() {
-		addReparation.execute();
-		
-		Customer customer = customers.getCustomerById(customerId);
-		Reparation reparation = customer.getReparation(deviceSerialNumber,
-				creationDate);
-		assertThat(reparation.getCreationDate()).isEqualTo(creationDate);
-	}
-	
 	@Test(expected = AddReparationInteractor.CustomerNotFound.class)
 	public void throwCustomerNotFoundWhenProvidedCustomerIdDontMatchWithAnyCustomer() {
 		addReparation.execute();
 	}
-	
-	@Test
-	public void CustomerIsFoundWhenExecuteAddOperation() {
-		Interactor addCustomer = new AddCustomerInteractor(customerId, customers);
-		addCustomer.execute();
-		addReparation.execute();
+
+	public class CustomerIsFoundWhenExecuteAddOperation {
+		@Before
+		public void givenCustomer() {
+			Interactor addCustomer = new AddCustomerInteractor(customerId,
+					customers);
+			addCustomer.execute();
+		}
+
+		@Ignore
+		@Test
+		public void testAddReparation() {
+			addReparation.execute();
+
+			Customer customer = customers.getCustomerById(customerId);
+			assertThat(customer).isNotNull();
+			assertThat(customer).isNotEqualTo(Customer.NULL);
+			
+			Reparation reparation = customer.getReparation(deviceSerialNumber,
+					creationDate);
+			assertThat(reparation).isNotNull();
+			assertThat(reparation.getCreationDate()).isEqualTo(creationDate);
+		}
 	}
 }
