437de803b960d06aad8c067155eaa9eabf9d2d4d
Damian Palpacelli <damian.palpacelli@gmail.com>
2014-10-01 21:34:32 -0300

    AddCustomerInteractor logic

--- service-domain/src/main/java/reparationservice/interactors/AddCustomerInteractor.java
+++ service-domain/src/main/java/reparationservice/interactors/AddCustomerInteractor.java
@@ -1,17 +1,21 @@
 package reparationservice.interactors;
 
+import reparationservice.entities.Customer;
 import reparationservice.gateways.CustomerGateway;
 
 public class AddCustomerInteractor implements Interactor {
 
+	private CustomerGateway customers;
+	private long customerId;
+
 	public AddCustomerInteractor(long customerId, CustomerGateway customers) {
-		// TODO Auto-generated constructor stub
+		this.customerId = customerId;
+		this.customers = customers;
 	}
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-
+		customers.addCustomer(Customer.newInstance(customerId));
 	}
 
 }
--- service-domain/src/test/java/reparationservice/interactors/AddCustomerInteractorTest.java
+++ service-domain/src/test/java/reparationservice/interactors/AddCustomerInteractorTest.java
@@ -3,29 +3,31 @@ package reparationservice.interactors;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import reparationservice.ReparationService;
+import reparationservice.entities.Customer;
 import reparationservice.gateways.CustomerGateway;
 
 public class AddCustomerInteractorTest {
 
-	private static final long CUSTOMER_ID = 0;
+	private static final long CUSTOMER_ID = 1;
 
 	@Before
 	public void setUp() {
 	}
 
-	@Ignore
 	@Test
 	public void creation() {
 		CustomerGateway customers = new ReparationService();
+
 		Interactor addCustomer = new AddCustomerInteractor(CUSTOMER_ID,
 				customers);
 		addCustomer.execute();
-		
-		assertThat(customers.getCustomerById(CUSTOMER_ID).getId()).isEqualTo(CUSTOMER_ID);
+
+		Customer recoveredCustomer = customers.getCustomerById(CUSTOMER_ID);
+		assertThat(recoveredCustomer).isNotNull();
+		assertThat(recoveredCustomer.getId()).isEqualTo(CUSTOMER_ID);
 	}
 
 }
