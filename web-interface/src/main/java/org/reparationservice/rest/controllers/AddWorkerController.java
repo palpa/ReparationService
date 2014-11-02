54b5e7341a780583cac169cbc973aa3e6d02d9af
unknown <d.palpacelli@WIN81-AT3M0L1S.corp.avanade.org>
2014-11-02 00:55:24 -0300

    Hateoas Error Handler when try to create duplicate Worker   Return http error code 409 (Conflict)

--- web-interface/src/main/java/org/reparationservice/rest/controllers/AddWorkerController.java
+++ web-interface/src/main/java/org/reparationservice/rest/controllers/AddWorkerController.java
@@ -2,22 +2,28 @@ package org.reparationservice.rest.controllers;
 
 import org.reparationservice.rest.requests.AddWorkerJsonRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.VndErrors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import reparationservice.gateways.WorkerGateway;
 import reparationservice.interactors.AddWorkerInteractor;
+import reparationservice.interactors.AddWorkerInteractor.WorkerAlreadyExists;
 import reparationservice.requests.AddWorkerRequest;
 import reparationservice.requests.Request;
 
 @RestController
 public class AddWorkerController {
   private WorkerGateway workerGW;
-  
+
   @Autowired
   public AddWorkerController(WorkerGateway workerGW) {
     this.workerGW = workerGW;
@@ -30,5 +36,17 @@ public class AddWorkerController {
     interactor.execute(request);
 
     return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+
+  @ControllerAdvice
+  public static class AddWorkerControllerAdvice {
+    @ResponseBody
+    @ExceptionHandler(WorkerAlreadyExists.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    VndErrors workerAlreadyExistsExceptionHandler(WorkerAlreadyExists ex) {
+      String message = (ex.getMessage() == null || ex.getMessage().isEmpty()) ?
+          "Worker Already Exists" : ex.getMessage();
+      return new VndErrors("error", message);
+    }
   }
 }
--- web-interface/src/test/java/org/reparationservice/rest/controllers/webappcontext/AddWorkerControllerTest.java
+++ web-interface/src/test/java/org/reparationservice/rest/controllers/webappcontext/AddWorkerControllerTest.java
@@ -1,6 +1,7 @@
 package org.reparationservice.rest.controllers.webappcontext;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -9,31 +10,46 @@ import org.junit.Test;
 import org.reparationservice.rest.requests.AddWorkerJsonRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import reparationservice.entities.Worker;
 import reparationservice.gateways.WorkerGateway;
 
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class AddWorkerControllerTest extends ControllerTest {
+  private static final String WORKERS_URI = "/workers";
   private static final String WORKER_USERNAME = "username";
+  private static final ResultMatcher CONFLICT_STATUS_EXPECTED = status().isConflict();
+  private static final ResultMatcher CREATED_STATUS_EXPECTED = status().isCreated();
+
   @Autowired
-  WorkerGateway workerGW;
+  private WorkerGateway workerGW;
 
   @Test
-  @DirtiesContext
-  public void addWorker() throws Exception {
-    String workerJson = json(AddWorkerJsonRequest.newInstance(WORKER_USERNAME));
-
+  public void successfulWorkerCreation() throws Exception {
     assertThat(workerGW.getWorkerByUserName(WORKER_USERNAME))
         .isEqualTo(Worker.NULL);
 
-    mockMvc.perform(
-        post("/workers")
-            .contentType(contentType)
-            .content(workerJson))
-        .andDo(print())
-        .andExpect(status().isCreated());
+    sendWorkerPostRequestFor(WORKER_USERNAME, CREATED_STATUS_EXPECTED);
 
     assertThat(workerGW.getWorkerByUserName(WORKER_USERNAME).getUserName())
         .isEqualTo(WORKER_USERNAME);
+  }
+
+  @Test
+  public void returnErrorTryingToAddAnExistingWorker() throws Exception {
+    sendWorkerPostRequestFor(WORKER_USERNAME, CREATED_STATUS_EXPECTED);
+    sendWorkerPostRequestFor(WORKER_USERNAME, CONFLICT_STATUS_EXPECTED);
+  }
+
+  private void sendWorkerPostRequestFor(String username, ResultMatcher expectedStatus)
+      throws Exception {
+    String workerJson = json(AddWorkerJsonRequest.newInstance(username));
+    mockMvc.perform(
+        post(WORKERS_URI)
+            .contentType(contentType)
+            .content(workerJson))
+        .andDo(print())
+        .andExpect(expectedStatus);
   }
 }
