fb9096eb7c755f95ff1d906eded7334b2ab82af1
unknown <d.palpacelli@WIN81-OSXATIH1.corp.avanade.org>
2014-12-06 17:38:18 -0300

    Remove not used Dynamic Requestor (2nd part)

--- service-domain/src/main/java/reparationservice/requestor/DynamicRequestBuilder.java
+++
@@ -1,7 +0,0 @@
-package reparationservice.requestor;
-
-import java.util.HashMap;
-
-public interface DynamicRequestBuilder {
-  public abstract UseCaseRequest build(String requestName, HashMap<String, Object> args);
-}
