f9a3763ff62c64663eda5acc763f29460f99beb2
unknown <d.palpacelli@WIN81-OSXATIH1.corp.avanade.org>
2014-12-18 09:48:00 -0300

    Replace custom Stubs in favor of Mockito

--- web-interface/src/test/java/org/reparationservice/rest/controllers/standalone/AddWorkerCtrlTest.java
+++ web-interface/src/test/java/org/reparationservice/rest/controllers/standalone/AddWorkerCtrlTest.java
@@ -1,66 +1,83 @@
 package org.reparationservice.rest.controllers.standalone;
 
+import static org.mockito.Mockito.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.reparationservice.rest.controllers.AddWorkerController;
 import org.reparationservice.rest.controllers.standalone.helpers.TestHelper;
-import org.reparationservice.rest.controllers.standalone.helpers.doubles.AddWorkerInteractorFactoryStub;
-import org.reparationservice.rest.controllers.standalone.helpers.doubles.AddWorkerRequestBuilderStub;
 import org.reparationservice.rest.requests.AddWorkerJsonRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import reparationservice.entities.worker.WorkerGateway;
-import reparationservice.entities.worker.WorkerGatewaySpy;
+import reparationservice.usecases.worker.add.AddWorkerInteractorFactory;
+import reparationservice.usecases.worker.add.AddWorkerRequestBuilder;
+import reparationservice.requestor.UseCaseActivator;
+import reparationservice.requestor.UseCaseRequest;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AddWorkerCtrlTest {
-  private static final String WORKER_USERNAME_1 = "username1";
-  private static final String WORKER_USERNAME_2 = "username2";
+  private static final String WORKER_USERNAME_1 = "any username 1";
+  private static final String WORKER_USERNAME_2 = "any username 2";
   private static final MediaType JSON_HAL_CONTENT_TYPE = TestHelper.JSON_HAL_CONTENT_TYPE;
   private MockMvc mockMvc;
-  private AddWorkerInteractorFactoryStub intFactoryStub;
-  private AddWorkerRequestBuilderStub requestBuilderStub;
+  @Mock
+  private AddWorkerInteractorFactory addWorkerIF;
+  @Mock
+  private AddWorkerRequestBuilder addWorkerRB;
+  @Mock
   private WorkerGateway workerGW;
+  @Mock
+  private UseCaseActivator interactor;
 
   @Before
   public void setup() throws Exception {
-    intFactoryStub = AddWorkerInteractorFactoryStub.newInstance();
-    requestBuilderStub = new AddWorkerRequestBuilderStub();
-    workerGW = new WorkerGatewaySpy();
-    AddWorkerController addWorkerCtrl = new AddWorkerController(intFactoryStub, workerGW, requestBuilderStub);
+    when(addWorkerIF.makeAddWorkerInteractor(any(WorkerGateway.class), any(UseCaseRequest.class)))
+        .thenReturn(interactor);
+
+    AddWorkerController addWorkerCtrl = new AddWorkerController(addWorkerIF, workerGW, addWorkerRB);
     mockMvc = MockMvcBuilders.standaloneSetup(addWorkerCtrl).build();
+
     sendWorkerPostRequestFor(WORKER_USERNAME_1);
   }
 
   @Test
   public void callToAddWorkerInteractorWithGivenGW() throws Exception {
-    assertThat(intFactoryStub.wasMakeAddWorkerInteractorCalled()).isTrue();
-    assertThat(intFactoryStub.getWorkerGateway()).isInstanceOf(workerGW.getClass());
+    ArgumentCaptor<WorkerGateway> wGwArg = ArgumentCaptor.forClass(WorkerGateway.class);
+    verify(addWorkerIF, times(1)).makeAddWorkerInteractor(wGwArg.capture(),
+        any(UseCaseRequest.class));
+
+    assertThat(wGwArg.getValue()).isSameAs(workerGW);
   }
 
   @Test
-  public void createdRequest() throws Exception {
-    assertRequestFor(WORKER_USERNAME_1);
+  public void interactorActionWasExecuted() throws Exception {
+    verify(interactor).execute();
+  }
+
+  @Test
+  public void moreThanOneRequest() throws Exception {
     sendWorkerPostRequestFor(WORKER_USERNAME_2);
-    assertRequestFor(WORKER_USERNAME_2);
-  }
-
-  @Test
-  public void executeWasCalled() throws Exception {
-    assertThat(intFactoryStub.wasExecuteCalled()).isTrue();
-  }
-
-  private void assertRequestFor(String workerUsername) {
-    assertThat(requestBuilderStub.wasAddWorkerRequestBuildCalled()).isTrue();
-    assertThat(requestBuilderStub.withArgs(workerUsername)).isTrue();
+    
+    ArgumentCaptor<String> usernameArg = ArgumentCaptor.forClass(String.class);
+    verify(addWorkerRB, times(2)).buildAddWorkerRequest(usernameArg.capture());
+    
+    List<String> unernames = usernameArg.getAllValues();
+    assertThat(unernames.get(0)).isEqualTo(WORKER_USERNAME_1);
+    assertThat(unernames.get(1)).isEqualTo(WORKER_USERNAME_2);
   }
 
   private void sendWorkerPostRequestFor(String username) throws Exception {
